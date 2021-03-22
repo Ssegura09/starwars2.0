a80510a27336b7a8698b9829ca29658b8e6f9b77
Ssegura09 <stephaniemsegura09@gmail.com>
2021-03-22 14:03:18 -0500

    fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+ 
 function App() {
+  const [data, setData] = useState({ hits: [] });
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://localhost:3000/api/v1/characters',
+      );
+ 
+      setData(result.data);
+    };
+ 
+    fetchData();
+  }, []);
+ 
   return (
-    <div>Hello There</div>
+    <ul>
+      {data.hits.map(item => (
+        <li key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
   );
 }
-
+ 
 export default App;
