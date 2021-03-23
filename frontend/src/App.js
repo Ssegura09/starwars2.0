72509c9833366cc49f5e77e14f8dae5aa59ce0db
Ssegura09 <stephaniemsegura09@gmail.com>
2021-03-23 14:00:57 -0500

    class or function component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
- 
+
+
 function App() {
-  const [data, setData] = useState({ hits: [] });
- 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        'https://localhost:3000/api/v1/characters',
-      );
- 
-      setData(result.data);
-    };
- 
-    fetchData();
-  }, []);
- 
   return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <div className="App">
+      <h1>hello there</h1>
+    </div>
   );
 }
- 
+
 export default App;
